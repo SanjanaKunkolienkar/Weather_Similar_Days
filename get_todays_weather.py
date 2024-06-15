0f9af8a9cd0651ccf884031c3ed9f766fd92966e
SanjanaKunkolienkar <99448403+SanjanaKunkolienkar@users.noreply.github.com>
2024-06-15 15:39:37 -0500

    Final Code

--- get_todays_weather.py
+++ get_todays_weather.py
@@ -1,28 +1,41 @@
 import cdsapi
+from datetime import date, timedelta
 
+class Request:
+    def __init__(self, __date: date) -> None:
+        self.date: date = __date - timedelta(10)                             # Latest data is 5 days ago
+        self.quarter: int = ((self.date.month - 1) // 3) + 1                # What quarter is the data from, e.g. Q1, Q2, etc.
+        self.file: str = f"Texas{self.date.year}_Q{self.quarter}"    # File name. e.g. NorthAmerica2024_Q1
+
+req = Request(date.today())
+print(f"Downloading data for {req.date.strftime('%d %B %Y')}")
 # Initialize the API client
-c = cdsapi.Client()
-
-# Define the API request for ERA5 data
-c.retrieve(
-    'reanalysis-era5-single-levels', {
-        'product_type': 'reanalysis',
-        'format': 'netcdf',  # You can choose other formats like GRIB
-        'variable': [
-            '2m_temperature', 'dew_point_temperature', '10m_u_component_of_wind',
-            '10m_v_component["WindSpeed100Avg", "WindSpeed100Min", "WindSpeed100Max"]', 'wind_speed', 'wind_direction',
-            'total_cloud_cover', 'surface_solar_radiation_downwards',
-        ],
-        'year': '2024',
-        'month': '06',
-        'day': '15',
-        'time': [
-            '00:00', '06:00', '12:00', '18:00'
-        ],
-        'area': [
-            36.5, -106.5, 25.5, -93.5,  # North, West, South, East coordinates for Texas
-        ],
-    },
-    'download.nc')  # Filename
-
-print("Data download is complete.")
+CDS = cdsapi.Client()
+CDS.retrieve(
+        'reanalysis-era5-single-levels',
+        {
+            'product_type': 'reanalysis',
+            'format': 'netcdf',
+            'variable': [
+                '2m_dewpoint_temperature', '2m_temperature',
+                '100m_u_component_of_wind', '100m_v_component_of_wind', '10m_u_component_of_wind',
+                '10m_v_component_of_wind', 'total_cloud_cover', 'high_cloud_cover', 'low_cloud_cover',
+                'medium_cloud_cover', 'surface_solar_radiation_downwards',
+                'total_sky_direct_solar_radiation_at_surface', 'geopotential'
+            ],
+            'area': [
+                '36', '-108', '24', '-90'
+            ],
+            'time': [
+                '00:00', '01:00', '02:00', '03:00', '04:00',
+                '05:00', '06:00', '07:00', '08:00', '09:00',
+                '10:00', '11:00', '12:00', '13:00', '14:00',
+                '15:00', '16:00', '17:00', '18:00', '19:00',
+                '20:00', '21:00', '22:00', '23:00',
+            ],
+            'day': [f'{req.date.day:02d}'],
+            'month': [f'{req.date.month:02d}'],
+            'year': f'{req.date.year}',
+        },
+        f"{req.file}.nc"
+    )
--- main.py
+++ main.py
@@ -38,7 +38,7 @@ result = pw_object.RunScriptCommand('SolvePowerFlow(DC);')
 logger.info(f"{CheckResultForError(result, 'Solved Power Flow')}")
 
 # get a list of files in the pww_filepath directory
-files = os.listdir(pww_filepath)
+#files = os.listdir(pww_filepath)
 
 var_list = ['Date', 'TimeDomainWeatherSummary']
 for i in range(1, 24):
@@ -48,32 +48,35 @@ df_results = pd.DataFrame(columns = ['Date', 'TempAvg', 'TempMin', 'TempMax', 'D
                   'WindSpeedAvg', 'WindSpeedMin', 'WindSpeedMax', 'WindDirAvg', 'WindDirMin', 'WindDirMax',
                   'CloudCoverAvg', 'CloudCoverMin', 'CloudCoverMax', 'WindSpeed100Avg', 'WindSpeed100Min', 'WindSpeed100Max',
                   'GlobHorIrradAvg', 'GlobHorIrradMin', 'GlobHorIrradMax', 'DirNormIrradAvg', 'DirNormIrradMin', 'DirNormIrradMax'])
-for file in files:
-    print("Filename: ", file[:-4])
-    pww_file = os.path.join(pww_filepath, file)
-    command = 'TimeStepAppendPWW("{}", Single Solution)'.format(pww_file)
-    result_timestep = pw_object.RunScriptCommand(command)
-    logger.info(f"{CheckResultForError(result_timestep, 'PWW file loaded successfully')}")
+#for file in files:
 
-    result_temp = pw_object.GetParametersMultipleElement('Timepoint', var_list, '')
-    logger.info(f"{CheckResultForError(result_temp, 'Data saved successfully')}")
-    data = result_temp[1]
-    df_data = pd.DataFrame({f"Column{i + 1}": [x.strip() if x is not None else None for x in column] for i, column in
-                              enumerate(data)})
-    df_data.columns = ['Date', 'TempAvg', 'TempMin', 'TempMax', 'DewPointAvg', 'DewPointMin', 'DewPointMax',
-                  'WindSpeedAvg', 'WindSpeedMin', 'WindSpeedMax', 'WindDirAvg', 'WindDirMin', 'WindDirMax',
-                  'CloudCoverAvg', 'CloudCoverMin', 'CloudCoverMax', 'WindSpeed100Avg', 'WindSpeed100Min', 'WindSpeed100Max',
-                  'GlobHorIrradAvg', 'GlobHorIrradMin', 'GlobHorIrradMax', 'DirNormIrradAvg', 'DirNormIrradMin', 'DirNormIrradMax']
-    print(df_data.tail(5))
-    #conver all columns except Date to float
-    df_data.iloc[:, 1:] = df_data.iloc[:, 1:].astype(float)
+#print("Filename: ", file[:-4])
+#pww_file = os.path.join(pww_filepath, file)
+pww_file = "D:/Github/Weather_Similar_Days/Texas_Q2_3.pww"
+command = 'TimeStepAppendPWW("{}", Single Solution)'.format(pww_file)
+result_timestep = pw_object.RunScriptCommand(command)
+logger.info(f"{CheckResultForError(result_timestep, 'PWW file loaded successfully')}")
 
-    command_del = 'TimeStepDeleteAll;'
-    result_timestep = pw_object.RunScriptCommand(command_del)
+result_temp = pw_object.GetParametersMultipleElement('Timepoint', var_list, '')
+logger.info(f"{CheckResultForError(result_temp, 'Data saved successfully')}")
+data = result_temp[1]
+df_data = pd.DataFrame({f"Column{i + 1}": [x.strip() if x is not None else None for x in column] for i, column in
+                          enumerate(data)})
+df_data.columns = ['Date', 'TempAvg', 'TempMin', 'TempMax', 'DewPointAvg', 'DewPointMin', 'DewPointMax',
+              'WindSpeedAvg', 'WindSpeedMin', 'WindSpeedMax', 'WindDirAvg', 'WindDirMin', 'WindDirMax',
+              'CloudCoverAvg', 'CloudCoverMin', 'CloudCoverMax', 'WindSpeed100Avg', 'WindSpeed100Min', 'WindSpeed100Max',
+              'GlobHorIrradAvg', 'GlobHorIrradMin', 'GlobHorIrradMax', 'DirNormIrradAvg', 'DirNormIrradMin', 'DirNormIrradMax']
+print(df_data.tail(5))
+#conver all columns except Date to float
+df_data.iloc[:, 1:] = df_data.iloc[:, 1:].astype(float)
 
-    df_byday = df_data.groupby('Date').mean()
-    # combine dataframe df_byday for all for loops
-    df_results = pd.concat([df_results, df_byday])
+command_del = 'TimeStepDeleteAll;'
+result_timestep = pw_object.RunScriptCommand(command_del)
 
-df_results.to_csv('D:/Github_extras/Texas_1940-2023/Texas_1940_2023.csv')
+df_byday = df_data.groupby('Date').mean()
+# combine dataframe df_byday for all for loops
+df_results = pd.concat([df_results, df_byday])
+print(df_results)
+#df_results.to_csv('D:/Github_extras/Texas_1940-2023/Texas_1940_2023.csv')
 
+del pw_object
